9bc34a7abcc6b1332de90defa13efd8a00a38892
Ilya Bakulin <ilya@bakulin.de>
2020-11-16 10:34:16 +0100

    Make it possible to push metrics instead of serving them via http
    It's necessary to configure a Push Gateway!
    
    But together with lowering the CPU freq and going to deep sleep
    between pushes it's possible to significantly lower power consumption.

--- bathroom_metrics.ino
+++ bathroom_metrics.ino
@@ -1,60 +1,83 @@
+#include <HTTPClient.h>
+#include <WebServer.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WebServer.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
+#include <driver/adc.h>
+#include <esp_wifi.h>
+#include <esp_bt.h>
 
 #include "wifi_settings.h"
 
+/* Pins for I2C comms */
 #define SDA1 21
 #define SCL1 22
+
+/* BME280 I2C address */
 #define BME280_ADDR 0x76
+
+/* Number of retries for pushing metrics to Push Gateway */
+#define MAX_PUSH_RETRIES 3
+
+/* Metrics push interval */
+#define PUSH_INTERVAL_SEC 300
 
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASS;
 
-WebServer server(80);
-
 float humidity;
 float temp;
 
-TwoWire i2cbus = TwoWire(0);
+WebServer server(80);
+TwoWire i2cbus(0);
 Adafruit_BME280 bme;
 
-void scan_i2cbus(){
-  Serial.println("Scanning I2C Addresses Channel 1");
-  uint8_t cnt = 0;
-  for (uint8_t i=0; i<128; i++) {
-    i2cbus.beginTransmission(i);
-    uint8_t  ec = i2cbus.endTransmission(true);
-    if (ec==0) {
-      if (i<16)
-        Serial.print('0');
-      Serial.print(i, HEX);
-      cnt++;
-     } else Serial.print("..");
-     Serial.print(' ');
-     if ( (i&0x0f)==0x0f)
-     Serial.println();
-  }
-  Serial.print("Scan Completed,");
-  Serial.print(cnt);
-  Serial.println(" I2C Devices found.");
+/* Generate metrics in Prometheus format */
+String getMetrics() {
+  return String(
+    "# HELP humidity Ambient humidity.\n" 
+      "# TYPE humidity gauge\n"
+      "humidity " + String(humidity) + "\n" + 
+      "# HELP temperature Ambient temperature.\n" + 
+      "# TYPE temperature gauge\n" + 
+      "temperature " + String(temp) + "\n"
+    );
 }
 
-void setup(void) {
-  Serial.begin(921600);
-  i2cbus.begin(SDA1, SCL1, 400000);
-  if (!bme.begin(BME280_ADDR, &i2cbus)) {
-    Serial.println("Cannot start BME sensor comms.");
-    return;
-  }
+/* Push metrics to PushGateway endpoint */
+bool pushMetrics() {
+  bool success = false;
+  Serial.println("Starting to push metrics to " PUSH_URL);
+  
+  HTTPClient http;
 
+  http.begin(PUSH_URL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpResponseCode = http.POST(getMetrics());
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+  if (httpResponseCode > 0)
+    success = true;
+  // Free resources
+  http.end();
+  return success;
+}
+
+/* Push metrics and retry if not successful */
+void pushMetricsRetry() {
+  for (int i = 1; i < MAX_PUSH_RETRIES; i++) {
+    if (pushMetrics())
+      break;
+  }
+}
+
+void connectNetwork() {
   WiFi.mode(WIFI_STA);
+  Serial.println("Starting to connect to the WiFi network");
   WiFi.begin(ssid, password);
-  Serial.println("");
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -65,28 +88,79 @@ void setup(void) {
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());  
+}
 
+/* Stop all power-hungry h/w, schedule time to wake up, and hibernate */
+void goSleep(int wakeupTimer) {
+  Serial.println("Going to sleep...");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  btStop();
+
+  adc_power_off();
+  esp_wifi_stop();
+  esp_bt_controller_disable();
+
+  // Configure the timer to wake us up!
+  esp_sleep_enable_timer_wakeup(wakeupTimer * 1000000L);
+
+  // Go to sleep! Zzzz
+  esp_deep_sleep_start();
+}
+
+void setupServer() {
+  
   server.on("/", []() {
     server.send(200, "text/plain", "<a href=\"/metric\"s>Prometheus metrics</a>");
   });
 
   server.on("/metrics", []() {
-    update_sensors();
-    String output = "# HELP humidity Ambient humidity.\n" 
-      "# TYPE humidity gauge\n"
-      "humidity " + String(humidity) + "\n" + 
-      "# HELP temperature Ambient temperature.\n" + 
-      "# TYPE temperature gauge\n" + 
-      "temperature " + String(temp) + "\n";
-    server.send(200, "text/plain", output);
+    updateSensors();
+    server.send(200, "text/plain", getMetrics());
+  });
+
+  server.on("/push", []() {
+    updateSensors();
+    pushMetricsRetry();
+    server.send(200, "text/plain", "Pushed.");
+  });
+
+  server.on("/zzz", []() {
+    server.send(200, "text/plain", "Going to sleep.");
+    goSleep(30);
   });
 
   server.begin();
   Serial.println("HTTP server started");
 }
 
-void update_sensors() {
+void setupHardware() {
+  setCpuFrequencyMhz(80); // 80 Mhz is minimum for WiFi to work
+  Serial.begin(921600);   // Should match settings in Arduino
+  i2cbus.begin(SDA1, SCL1, 400000);
+  if (!bme.begin(BME280_ADDR, &i2cbus)) {
+    Serial.println("Cannot start BME sensor comms.");
+    return;
+  }  
+}
+
+const bool serverMode = false;
+
+void setup(void) {
+  setupHardware();
+  connectNetwork();
+  if (serverMode) {
+    setupServer();
+  } else {
+    updateSensors();
+    pushMetricsRetry();
+    goSleep(PUSH_INTERVAL_SEC);
+    /* NOTREACHED */
+  }
+}
+
+void updateSensors() {
   Serial.println(F("Begin to read from BMP sensor"));
   humidity = bme.readHumidity();
   temp = bme.readTemperature();
